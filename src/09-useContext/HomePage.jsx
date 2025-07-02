587d2cb764327fce91535995d44f11b56301ed2f
Juan Carlos Santamaría Villegas <43381558+jsantamvcr@users.noreply.github.com>
2025-07-01 19:46:35 -0600

    create context

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './context/UserContext.jsx';
 
 export const HomePage = () => {
+
+    const { user } = useContext(UserContext);
+
     return (
         <>
          <h1>Home  Page</h1>
+
+
+            <pre>{JSON.stringify(user, null, 2)}</pre>
         </>
     );
 };
--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './context/UserContext.jsx';
 
 export const LoginPage = () => {
+
+    const { user, setUser } = useContext(UserContext);
+
+
     return (
         <>
-            <h1>Login Page</h1>
+            <h1>Login Page <small>{user?.name}</small></h1>
+
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+
+            <button className="btn btn-primary"
+                    onClick={() => setUser({
+                        id: 123,
+                        name: 'John Doe',
+                        email: 'jsanta@asdfsd.com',
+                    })}
+            >Set User
+            </button>
+
         </>
+
     );
 };
 
