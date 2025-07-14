a5d1a5b86afe4d1b8579851ad720bb3ee0ccfbb6
Juan Carlos Santamaría Villegas <43381558+jsantamvcr@users.noreply.github.com>
2025-07-13 21:02:47 -0600

    testing

--- src/tests/hooks/useForm.test.js
+++ src/tests/hooks/useForm.test.js
@@ -1,6 +1,59 @@
-
 /* eslint-disable no-undef */
 
-describe('useForm hook', () => {
+import { act, renderHook } from '@testing-library/react';
+import { useForm } from '../../hooks/index.js';
+
+describe('Test useForm hook', () => {
+
+    const initialForm = {
+        name: 'Juan Carlos',
+        email: 'asdfasdf@safasd.com'
+    }
+
+    test('should return default values', () => {
+
+        const { result } = renderHook(() => useForm(initialForm));
+
+        expect(result.current).toEqual({
+            name: initialForm.name,
+            email: initialForm.email,
+            formState: initialForm,
+            onInputChange: expect.any(Function),
+            onResetForm: expect.any(Function)
+        });
+    });
+
+    // test for onInputChange change name value
+    test('should change name value', () => {
+
+        const newValue = 'Juan Carlos 2';
+        const { result } = renderHook(() => useForm(initialForm));
+        const { onInputChange, onResetForm } = result.current;
+
+        // act simulate input change
+        act(() => {
+            onInputChange({ target: { name: 'name', value: newValue } });
+        });
+
+        expect(result.current.name).toBe(newValue);
+        expect(result.current.formState.name).toBe(newValue);
+
+    });
+
+    test('should change name value', () => {
+
+        const newValue = 'Juan Carlos';
+        const { result } = renderHook(() => useForm(initialForm));
+        const { onResetForm } = result.current;
+
+        // act simulate input change
+        act(() => {
+            onResetForm();
+        });
+
+        expect(result.current.name).toBe(newValue);
+        expect(result.current.formState.name).toBe(newValue);
+
+    });
 
 })
